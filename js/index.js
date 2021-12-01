dffc73adf79abbad59916511c4a664e09cfa6ced
Rohit prasad <rpchauhan4444@gmail.com>
2021-12-01 20:20:39 +0530

    Minor Improvements regarding navigation

--- js/index.js
+++ js/index.js
@@ -24,3 +24,21 @@ ScrollReveal().reveal(".second-work", { delay: 400 });
 ScrollReveal().reveal(".skills", { delay: 400 });
 ScrollReveal().reveal("#coding-profiles", { delay: 400 });
 ScrollReveal().reveal(".contact-me", { delay: 400 });
+
+// const section = document.querySelector("header");
+
+// let currentPos = window.pageYOffset;
+
+// const update = () => {
+//   const newPos = window.pageYOffset;
+//   const diff = newPos - currentPos;
+//   const speed = diff * 0.25;
+
+//   section.style.transform = `skewY(${speed}deg)`;
+
+//   currentPos = newPos;
+
+//   requestAnimationFrame(update);
+// };
+
+// update();
