360bed833c654a5f1fef4fbe128fd27e37f9386e
Rohit Prasad <rpchauhan4444@gmail.com>
2024-05-24 17:23:51 +0530

    Fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/home";
 import Blog from "./pages/blog";
 
